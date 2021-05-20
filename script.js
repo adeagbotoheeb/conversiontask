21498f06e13d6d5b6d8a21fc4179660a7dc103f5
adeagbotoheeb <adeagbotoheeb1@gmail.com>
2021-05-20 09:00:47 +0100

    First commit

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ function convertFahrToCelsius(Fahr) {
 
     if (Fahr == null) {
         console.log('please Enter an input value of farenheit for conversion')
+        return('please Enter an input value of farenheit for conversion')
     }
     else {
         let regex = /[a-zA-Z]/
         regexResult = regex.test(Fahr)
         if (regexResult === true && typeof Fahr !== 'object' && Fahr !== null) {
             console.log('"${Fahr}"', 'is not a valid number but a/an ${typeof Fahr}' )
+            return('"${Fahr}"', 'is not a valid number but a/an ${typeof Fahr}' )
+
 
         }
         else if (Array.isArray(Fahr) === true) {
             console.log(Fahr, 'is not a valid number but a/an array')
+            return(Fahr, 'is not a valid number but a/an array')
         }
         else if (typeof Fahr === 'object' && typeof Fahr !== null && !Array.isArray(Fahr)) {
             console.log(Fahr, 'is not a valid number but a/an object')
+            return(Fahr, 'is not a valid number but a/an object')
 
         }
         else {
@@ -24,6 +29,7 @@ function convertFahrToCelsius(Fahr) {
 
             let C = ((F-32) * 100) / 180
             console.log(C.toFixed(4));
+            return(C.toFixed(4));
             
         }
     }
@@ -36,18 +42,22 @@ checkYuGiOh = (n) => {
 
     if (n == null) {
         console.log('please Enter an input value for testing')
+        return('please Enter an input value for testing')
     }
     else {
         let regex = /[a-zA-Z]/
         regexResult = regex.test(n)
         if (regexResult === true && typeof n !== 'object') {
             console.log('invalid parameter:', '${n}', 'is a/an ${typeof n}');
+            return('invalid parameter:', '${n}', 'is a/an ${typeof n}');
         }
         else if (Array.isArray(n) === true) {
             console.log('invalid parameter:', n, 'is an array');
+            return('invalid parameter:', n, 'is an array');
         }
         else if (typeof n === 'object' && typeof n !== null && !Array.isArray(n)) {
             console.log('invalid parameter:', n, 'is an object');
+            return('invalid parameter:', n, 'is an object');
         }
         else {
 
@@ -82,6 +92,7 @@ checkYuGiOh = (n) => {
 
             }
             console.log(result);
+            return(result);
 
         }
 
